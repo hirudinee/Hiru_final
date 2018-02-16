2463b1459e390a395b79ad0ba30848fbbf05c8f2
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-16 13:36:07 +0530

    test

--- Hiru_final/testlambda.js
+++ Hiru_final/testlambda.js
@@ -1,25 +1,17 @@
 let AWS = require('aws-sdk');
 const sns = new AWS.SNS();
 exports.handler = function (event, context, callback) {
-	sns.publish({
-		Message: 'Test sample',
-		MessageAttributes: {
-			'AWS.SNS.SMS.SMSType': {
-				DataType: 'String',
-				StringValue: 'Promotional'
-			},
-			'AWS.SNS.SMS.SenderID': {
-				DataType: 'String',
-				StringValue: 'test'
-			},
-		},
-		PhoneNumber: '+94715397214'
+
+	sns.subscribe({
+		Protocol: 'email',
+		Endpoint: 'hirudineel@gmail',
+		TopicArn: 'arn:aws:sns:us-east-2:480964559519:Hiru'
 	}).promise()
 		.then(data => {
-			console.log(data);// your code goes here
+			// your code goes here
 		})
 		.catch(err => {
-			console.log(err);// error handling goes here
+			// error handling goes here
 		});
 
 
